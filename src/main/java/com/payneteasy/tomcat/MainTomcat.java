54a80e48874e0e77d8f9b657a7a0f3063bec864c
Evgeniy <evsinev@gmail.com>
2014-04-07 23:32:49 +0400

    Added apr, bio and nio for tomcat

--- src/main/java/com/payneteasy/tomcat/MainTomcat.java
+++ src/main/java/com/payneteasy/tomcat/MainTomcat.java
@@ -1,17 +1,44 @@
 package com.payneteasy.tomcat;
 
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.AprLifecycleListener;
+import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http11.Http11NioProtocol;
 
 import javax.servlet.ServletException;
 import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class MainTomcat {
 
-    public static void main(String[] args) throws LifecycleException, ServletException {
+    public static void main(String[] args) throws LifecycleException, ServletException, UnknownHostException {
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+        // will be apr
+        tomcat.setPort(9008);
+
         tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
+
+        Connector nioConnector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        nioConnector.setPort(9006);
+        nioConnector.setProperty("address", InetAddress.getByName("127.0.0.1").getHostAddress());
+        tomcat.getService().addConnector(nioConnector);
+
+        // Add AprLifecycleListener
+        StandardServer server = (StandardServer)tomcat.getServer();
+        AprLifecycleListener listener = new AprLifecycleListener();
+        server.addLifecycleListener(listener);
+
+        Connector aprConnector = new Connector("org.apache.coyote.http11.Http11Protocol");
+        aprConnector.setProperty("SSLEnabled", "false");
+        aprConnector.setProperty("secure", "false");
+        aprConnector.setProperty("scheme", "http");
+        aprConnector.setPort(9007);
+
+        tomcat.getService().addConnector(aprConnector);
+
         tomcat.start();
         tomcat.getServer().await();
     }
